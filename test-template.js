c1cbb9ab5cde2656e50a09fae63139b43e76e807
danielVg <danielveragi@gmail.com>
2021-03-12 17:28:28 +0100

    change test template to use querying from github

--- test-template.js
+++ test-template.js
@@ -1,61 +1,58 @@
 const token = pm.response.json().token
+const { getNeoTestingTools } = eval(environment.neoTestingTools)()
 let replyId = null
-const { getTestingFunctions, looseJsonParse } = eval(environment.neoTestingTools)()
-getTestingFunctions()
-  .then((res) => {
-    let neoTestingFunctions = looseJsonParse(res)
-    // Neo hilfsfunktionen:
-    // TIPP: Man kann diese links am editor "einklappen" :)
-    const {
-      sendMessage,
-      sendReply,
-      sendAction,
-      sendRequest,
-      isResponseOk,
-      isReprompt,
-      isContentType,
-      showsAdaptiveCard,
-      showsDownload,
-      showsExpandable,
-      showsHtml,
-      showsImage,
-      showsMap,
-      showsMedia,
-      showsText,
-      triggersAudio,
-      triggersCamera,
-      triggersSuggestion,
-      triggersUpload,
-      isDirective,
-      doesPlayAudio,
-      doesCopyToClipboad,
-      doesComposeAnEmail,
-      doesPhoneCall,
-      doesOpenAnUrl,
-      isSticky,
-      isIntent,
-      getKeys,
-      fuzzyDataSearchTest,
-    } = neoTestingFunctions
 
-    // TODO 0. OPTIONAL: LOCALE ÄNDERN
-    const locale = 'de-DE'
-    // TODO 1. INSERT WORKSPACE NAME HERE
-    const workspace = '[WORKSPACE_NAME]'
+// TODO 0. OPTIONAL: LOCALE ÄNDERN
+const locale = 'de-DE'
+// TODO 1. INSERT WORKSPACE NAME HERE
+const workspace = 'cypher'
 
-    async function main() {
-      let particle
+async function main() {
+  let particle
+  const neoTestingTools = await getNeoTestingTools()
+  // Neo hilfsfunktionen:
+  // TIPP: Man kann diese links am editor "einklappen" :)
+  const {
+    sendMessage,
+    sendReply,
+    sendAction,
+    sendRequest,
+    isResponseOk,
+    isReprompt,
+    isContentType,
+    showsAdaptiveCard,
+    showsDownload,
+    showsExpandable,
+    showsHtml,
+    showsImage,
+    showsMap,
+    showsMedia,
+    showsText,
+    triggersAudio,
+    triggersCamera,
+    triggersSuggestion,
+    triggersUpload,
+    isDirective,
+    doesPlayAudio,
+    doesCopyToClipboad,
+    doesComposeAnEmail,
+    doesPhoneCall,
+    doesOpenAnUrl,
+    isSticky,
+    isIntent,
+    getKeys,
+    fuzzyDataSearchTest,
+  } = eval(neoTestingTools)()
 
-      // TODO 2. ADD TEST CASES HERE
+  // TODO 2. TEST ERSTELLEN.
+  // SIEHE: https://github.com/neohelden/flow-testing-manual
 
-      return 'Done'
-    }
+  return 'Done'
+}
 
-    // Do not remove the lines down below; required for async tests
-    const interval = setTimeout(() => {}, 50000)
-    main().then(() => {
-      console.log('Completely done! 🚀')
-      clearTimeout(interval)
-    })
-  })
-  .catch((err) => console.error(err))
+// Do not remove the lines down below; required for async tests
+const interval = setTimeout(() => {}, 50000)
+main().then(() => {
+  console.log('Completely done! 🚀')
+  clearTimeout(interval)
+})
